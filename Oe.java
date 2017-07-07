6b1cb389457e106ff5f28c34ab164541e3269be3
SAMLEVENSON <samjesus241@gmail.com>
2017-07-07 12:33:39 +0530

    Create Oe.java

---
+++ Oe.java
@@ -0,0 +1,20 @@
+import java.io.*;
+import java.util.*;
+public class Oe
+{
+public static void main(String [] args)
+{
+int a ;
+System.out.println("Enter a number to check whether its odd or even");
+Scanner sc= new Scanner(System.in);
+ a= sc.nextInt();
+if( a%2==0)
+{
+System.out.println("The number you have entered is Even " );
+}
+else
+{
+System.out.println("The number you have entered is Odd " );
+}
+}
+}
