1413a4cd230e704890089702d4d92e62c73df61d
SAMLEVENSON <samjesus241@gmail.com>
2018-02-23 13:43:04 +0530

    Update and rename Fact.py to Factorialwi.py

--- Factorialwi.py
+++ Factorialwi.py
@@ -1,9 +1,10 @@
 def main():
     print("To find the Factorial of a Number")
     a= int(input("Enter the Number:"))
-    factorial =1
-    for i in range(1,a + 1):
-       factorial = factorial*i
-    print(factorial)
+    if(a>=20)
+       fact =1
+       for i in range(1,a + 1):
+       fact = fact*i
+    print(fac)
 if __name__ == '__main__':
     main()
