9176c56840aa346cab6c7581a93f79c5a6bf2c9a
SAMLEVENSON <samjesus241@gmail.com>
2018-02-23 12:23:17 +0530

    Create oddnumbers.py

---
+++ oddnumbers.py
@@ -0,0 +1,8 @@
+def main():
+    a=int(input("Enter the First Digit:"))
+    b=int(input("Enter the Second Digit"))
+    for num in range(a,b + 1):
+      if(num%2!=0):
+        print(num)
+if __name__ == '__main__':
+    main()
